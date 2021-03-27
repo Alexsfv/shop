11c208a35fa12519bd22eae948a03289eacd156e
Alex <safronov.a@brightfit.ru>
2021-03-27 21:15:44 +0500

    models and routers

--- backend/app.js
+++ backend/app.js
@@ -2,31 +2,49 @@ require('dotenv/config')
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
 
 const app = express()
 const api = process.env.API_URL
+app.use(cors())
+app.options('*', cors())
 
+// Schemas
+const Product = require('./models/product')
 
+// Routes
+const productsRouter = require('./routers/products')
+const usersRoutes = require('./routers/users')
+const ordersRoutes = require('./routers/orders')
+const categoriesRoutes = require('./routers/categories')
 
+// Middleware
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
 
-// http://localhost:3080/api/v1/products
-app.get(`${api}/products`, (req, res) => {
-    const products = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
-    }
-    res.send(products)
-})
+// Routers
+app.use(`${api}/products`, productsRouter)
+app.use(`${api}/users`, usersRoutes)
+app.use(`${api}/categories`, categoriesRoutes)
+app.use(`${api}/orders`, ordersRoutes)
 
-app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body
-    console.log('New product', newProduct)
-    res.send(newProduct)
+
+// DB connect
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'shop-database',
+    useFindAndModify: false,
 })
+    .then(() => {
+        console.log('DB connected success')
+    })
+    .catch(err => {
+        console.log('DB error connect', err);
+    })
 
 
 app.listen(3080, () => { console.log('Server started http://localhost:3080/') })
